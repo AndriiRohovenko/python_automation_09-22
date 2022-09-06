b51c4369aac60ba2047b83fdb90066947da01f8d
Nikita <nikita@Nikita.Machine>
2022-09-06 22:27:12 +0200

    add alternative solution for read/write bytes from/to structure

---
+++ lesson_23/binary_example/read_write_bytes_to_file_using_bytes_constructor.py
@@ -0,0 +1,14 @@
+# numbers = [1, 2, 3, 4, 5]
+
+# write bytes into the file
+# with open("./data_new.txt", "wb") as file:
+#     file.write(
+#         bytes(numbers)
+#     )
+
+
+
+# read bytes from file and convert back to structure
+with open("./data_new.txt", "rb") as file:
+    bytes = file.read()
+    print(list(bytes))
